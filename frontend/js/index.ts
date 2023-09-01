a8742c882eb2f82049ab36b92bd95c98f969c051
Tyler Hallada <tyler@hallada.net>
2023-09-01 19:59:22 -0400

    Install htmx via bun instead of unpkg CDN

--- frontend/js/index.ts
+++ frontend/js/index.ts
@@ -1,4 +1,15 @@
+import htmx from 'htmx.org';
+import 'htmx.org/dist/ext/sse';
+
 // import CSS so it gets named with a content hash that busts caches
 import '../css/styles.css';
 
 import './localTimeController';
+
+declare global {
+  interface Window {
+    htmx: typeof htmx;
+  }
+}
+
+window.htmx = htmx;
